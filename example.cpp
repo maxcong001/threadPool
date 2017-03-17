048ab76ba779cd2bdf420c047f78d90c253607f0
Max <savagecm@users.noreply.github.com>
2017-03-17 16:42:31 +0800

    Update example.cpp

--- example.cpp
+++ example.cpp
@@ -1,40 +1,81 @@
+#include <iostream>
+#include <vector>
+#include <chrono>
+#include <map>
 #include <thread>
-
+#include <string>
+#include <tuple>
 #include "ThreadPool.hpp"
 
-int test(int i)
+using namespace std;
+typedef tuple<string, int> M_TUPLE;
+typedef std::function<M_TUPLE()> M_FUNCTION;
+vector< std::function<tuple<string, int>()> > M_VECTOR;
+typedef std::lock_guard<std::recursive_mutex> M_GUARD;
+typedef std::unique_lock<std::recursive_mutex> M_UNIQUE;
+std::recursive_mutex func_mutex;
+
+M_TUPLE M_put()
 {
-                std::cout << "hello " << i << '\n'<< std::flush;
-                std::this_thread::sleep_for(std::chrono::microseconds(i));//seconds(1));
-                std::cout << "world " << i << '\n' <<std::flush;
-                return i*i;
+    std::string M_func(__func__);
+
+    std::cout << "hello " << '\n'<< std::flush;
+    std::this_thread::sleep_for(std::chrono::microseconds(100));//seconds(1));
+    std::cout << "world "  << '\n' <<std::flush;
+    return std::make_tuple(M_func, 1);
 }
 
 int main()
 {
-
     int thread_num = std::thread::hardware_concurrency();
     if (!thread_num)
     {
         thread_num = 2;
     }
+    // now Jsaon's code does not support multi-client. So set the thread pool to one thread
+    thread_num = 1;
+    M_VECTOR.push_back(M_put);
+    M_VECTOR.push_back(M_put);
+    M_VECTOR.push_back(M_put);
+    M_VECTOR.push_back(M_put);
+
     std::cout<< " start "<< thread_num << "threads"<<std::endl;
 
     ThreadPool pool(thread_num);
-    std::vector< std::future<int> > results;
+    std::vector< std::future<M_TUPLE> > results;
+    M_FUNCTION tmp;
 
-    for(int i = 0; i < 80; ++i) {
+    while(!M_VECTOR.empty()) 
+    {
+        {
+            M_GUARD lock1(func_mutex);
+            tmp = M_VECTOR.back();
+        }
+
+
         results.emplace_back(
-            pool.enqueue([i] {
-                return test(i);
+        pool.enqueue([&] {
+            return tmp();
             })
         );
+        {
+            M_GUARD lock1(func_mutex);
+            M_VECTOR.pop_back();
+        }
+
     }
 
+
     for(auto && result: results)
-        std::cout << result.get() << '\n';
+    {
+        std::string tmp_str;
+        int tmp_bool;
+        tie(tmp_str, tmp_bool) = result.get();
+        cout << "string is "<< tmp_str<< "bool is "<<tmp_bool<<endl;
+    }
+
     std::cout << std::flush;
 
     return 0;
 }
-      
+
